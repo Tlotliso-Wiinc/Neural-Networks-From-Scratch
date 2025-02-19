f66df6a3df7847a11cdabc94a6f7ce680984dff1
Tlotliso Mafantiri <michaelmafantiri@gmail.com>
2025-02-19 19:07:20 +0200

    Add Backpropagation backward pass method on the ReLU activation class

--- ReLUActivation.py
+++ ReLUActivation.py
@@ -2,5 +2,15 @@ import numpy as np
 
 class ReLUActivation:
     def forward(self, inputs):
+        # Remember input values
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
         return self.output
+    
+    def backward(self, dvalues):
+        # Since we need to modify the original variable,
+        # let's make a copy of the values first
+        self.dinputs = dvalues.copy()
+        # Zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
+        return self.dinputs

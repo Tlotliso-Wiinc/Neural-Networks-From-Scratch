add8b7f164c9c9684e73fe342285096596ad7a93
Tlotliso Mafantiri <michaelmafantiri@gmail.com>
2025-02-25 18:36:13 +0200

    Add momemtum to the gradient descent optimizer

--- GradientDescentOptimizer.py
+++ GradientDescentOptimizer.py
@@ -1,12 +1,15 @@
+import numpy as np
+
 # SGD optimizer
 class GradientDescentOptimizer:
-    # Initialize optimizer - set settings,
+     # Initialize optimizer - set settings,
     # learning rate of 1. is default for this optimizer
-    def __init__(self, learning_rate=1., decay=0.):
+    def __init__(self, learning_rate=1., decay=0., momentum=0.):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
         self.iterations = 0
+        self.momentum = momentum
 
     # Call once before any parameter updates
     def pre_update_params(self):
@@ -16,8 +19,35 @@ class GradientDescentOptimizer:
 
     # Update parameters
     def update_params(self, layer):
-        layer.weights += -self.current_learning_rate * layer.dweights
-        layer.biases += -self.current_learning_rate * layer.dbiases
+        # If we use momentum
+        if self.momentum:
+            # If layer does not contain momentum arrays, create them
+            # filled with zeros
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                layer.bias_momentums = np.zeros_like(layer.biases)
+
+            # Build weight updates with momentum - take previous
+            # updates multiplied by retain factor and update with
+            # current gradients
+            weight_updates = self.momentum * layer.weight_momentums - \
+                             self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
+
+            # Build bias updates
+            bias_updates = self.momentum * layer.bias_momentums - \
+                           self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+
+        # Vanilla SGD updates (as before momentum update)
+        else:
+            weight_updates = -self.current_learning_rate * layer.dweights
+            bias_updates = -self.current_learning_rate * layer.dbiases
+
+        # Update weights and biases using either
+        # vanilla or momentum updates
+        layer.weights += weight_updates
+        layer.biases += bias_updates
 
     # Call once after any parameter updates
     def post_update_params(self):

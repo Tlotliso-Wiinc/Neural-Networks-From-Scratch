d0b5f51d9e4ae395e3af943e7a6fde1222d69f49
Tlotliso Mafantiri <michaelmafantiri@gmail.com>
2025-03-06 13:51:05 +0200

    Add the regularization loss to the Loss class

--- Loss.py
+++ Loss.py
@@ -3,6 +3,23 @@ import numpy as np
 # Common loss class
 class Loss:
 
+    def regularization_loss(self, layer):
+        # 0 by default
+        regularization_loss = 0
+        # L1 regularization - weights
+        if layer.weight_regularizer_l1 > 0:
+            regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+        # L2 regularization - weights
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 * np.sum(np.square(layer.weights))
+        # L1 regularization - biases
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 * np.sum(np.abs(layer.biases))
+        # L2 regularization - biases
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 * np.sum(np.square(layer.biases))
+        return regularization_loss
+
     # Calculates the data and regularization losses
     # given model output and ground truth values
     def calculate(self, output, y):
